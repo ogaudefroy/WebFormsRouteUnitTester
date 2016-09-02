c2aa4e9149d7598447112e6e9314d4d7facc6380
Olivier Gaudefroy <ogaudefroy>
2016-09-02 15:03:37 +0200

    Sample documentation.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # WebFormsRouteUnitTester
-A library for unit testing ASP.Net routing tables for WebForms
+A library for unit testing ASP.Net routing tables for WebForms. 
+Supports inbound and outbound route testing
+
+## Initialize RouteTester
+```C#
+var routes = new RouteCollection();
+routes.Ignore("content/{*content}");
+routes.MapPageRoute("Cookies", "cookies", "~/pages/cookies.aspx");
+routes.MapPageRoute("ItemDetails", "{culture}/items/{id}", "~/pages/items/details.aspx", false, new RouteValueDictionary() { { "culture", "en-US" } });
+var tester = new RouteTester(routes);
+
+```
+
+## Inbound route testing
+
+ - Without parameters
+
+```C#
+tester.WithIncomingRequest("/cookies")
+      .ShouldMatchPageRoute("~/Pages/General/Cookies.aspx");
+```
+ - With parameters
+```C#
+tester.WithIncomingRequest("en-US/items/33")
+      .ShouldMatchPageRoute("~/pages/items/details.aspx", new { culture = "en-US", id = 33 });
+```
+- Ignored routes
+```C#
+tester.WithIncomingRequest("content/bg666")
+      .ShouldBeIgnored();
+```
+- Missing routes
+```C#
+tester.WithIncomingRequest("fake")
+      .ShouldMatchNoRoute();
+```
+
+## Outbound route testing
+- Without parameters
+```C#
+tester.WithRouteInfo("Cookies")
+    .ShouldGenerateUrl("/cookies");
+```
+- With parameters
+```C#
+tester.WithRouteInfo("ItemDetails", new { id = 13, culture = "fr-FR"})
+    .ShouldGenerateUrl("/fr-FR/items/13");
+```
+- Parameter default values testing
+```C#
+tester.WithRouteInfo("ItemDetails", new { id = 13})
+    .ShouldGenerateUrl("/en-US/items/13");
+```
+Inspired by [MvcRouteUnitTester](http://mvcrouteunittester.codeplex.com/) 
---
+++ WebFormsRouteUnitTester.Tests/ExampleTest.cs
@@ -0,0 +1,50 @@
+﻿namespace WebFormsRouteUnitTester.Tests
+{
+    using System.Web.Routing;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ExampleTest
+    {
+        private RouteTester CreateRouteTester()
+        {
+            var routes = new RouteCollection();
+            routes.Ignore("content/{*content}");
+            routes.MapPageRoute("Cookies", "cookies", "~/pages/cookies.aspx");
+            routes.MapPageRoute("ItemDetails", "{culture}/items/{id}", "~/pages/items/details.aspx", false, new RouteValueDictionary(new { culture = "en-US" }));
+            return new RouteTester(routes);
+        }
+
+        [Test]
+        public void TestInboundRoutes()
+        {
+            var tester = CreateRouteTester();
+
+            tester.WithIncomingRequest("content/item")
+                .ShouldBeIgnored();
+
+            tester.WithIncomingRequest("cookies")
+                .ShouldMatchPageRoute("~/pages/cookies.aspx");
+            
+            tester.WithIncomingRequest("de-DE/items/33")
+                .ShouldMatchPageRoute("~/pages/items/details.aspx", new { culture = "de-DE", id = 33 });
+
+            tester.WithIncomingRequest("fake").ShouldMatchNoRoute();
+        }
+
+        [Test]
+        public void TestOutboundRoutes()
+        {
+            var tester = CreateRouteTester();
+
+            tester.WithRouteInfo("Cookies")
+                .ShouldGenerateUrl("/cookies");
+
+            tester.WithRouteInfo("ItemDetails", new { id = 13})
+                .ShouldGenerateUrl("/en-US/items/13");
+
+            tester.WithRouteInfo("ItemDetails", new { id = 13, culture = "fr-FR"})
+                .ShouldGenerateUrl("/fr-FR/items/13");
+        }
+    }
+}
